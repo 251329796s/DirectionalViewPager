6362c69c7e9391a64b7c04473d30e74bc205b67e
woody <553775169@qq.com>
2016-02-18 12:38:10 +0800

    sdf

--- sample/src/com/directionalviewpager/sample/SampleActivity.java
+++ sample/src/com/directionalviewpager/sample/SampleActivity.java
@@ -13,6 +13,7 @@ public class SampleActivity extends FragmentActivity {
         setContentView(R.layout.main);
 
         // 我就试一下
+        // 我再试一下
         //Set up the pager
         final DirectionalViewPager pager = (DirectionalViewPager)findViewById(R.id.pager);
         pager.setAdapter(new TestFragmentAdapter(getSupportFragmentManager()));
